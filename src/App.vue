09147cac20e646f999149bf0d4a1175dc3d9d018
Dmitriy Makuhin <typedefinc@gmail.com>
2023-10-14 19:45:26 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="application-wrapper">
+    <div class="router-window">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.application-wrapper {
+  @apply w-[100vw] h-[100vh];
+  @apply bg-gray-800;
+  @apply flex items-center justify-center;
+
+  .router-window {
+    @apply w-2/3 h-2/3;
+    @apply bg-gray-700;
+    @apply rounded-xl shadow-xl;
+    @apply text-white;
+  }
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="home-page">
+    <chat-list />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.home-page {
+
+}
+</style>
